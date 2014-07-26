aa4ff73ef38933203e72b2dd54d03e63273d73c7
Jackson Tian <shyvo1987@gmail.com>
2014-07-26 22:49:10 +0800

    Implmentation lexer

---
+++ test.js
@@ -0,0 +1,6 @@
+var foo = "Hello world";
+var bar = 10;
+console.log(foo + bar);
+var a = "abc";
+// comment
+console.log(1 + 1);
---
+++ token.js
@@ -0,0 +1,6 @@
+var Token = function () {
+  this.type = '';
+  this.value = '';
+  this.location = '';
+};
+Token.EOF = '\0';
